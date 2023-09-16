bc77343d9e7da1241b29ca0d0eda615907ffa5ef
IvanArechioga182 <ivanarechiga02@gmail.com>
2023-09-15 20:33:01 -0600

    Agregado el footer, tambien una frase aleatoria de bienvenida cada que la pagina se recarga y ejemplo del grid de las imagenes

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 'use strict'
+
+var frases = ['Diviértete hoy con lo nuevo de nuestra galería.',
+ 'Ilustra tu día y llena de inspiración tu mente con estas obras.',
+ "El arte es la expresión suprema del alma humana.",
+ "El arte es la búsqueda de la belleza en todas sus formas.",
+ "Sumérgete en el mundo del color y la forma.",
+ "Cada visita a una galería es un nuevo descubrimiento."];
+function fraseRandom(){
+    var indice = Math.floor(Math.random() * frases.length);
+    return frases[indice];
+}
+
+var elementoFrase = document.getElementById("fraseAleatoria");
+elementoFrase.textContent = fraseRandom();
